5b2e286a6bc8e94e3ba1f74bb32c612c9b7bfb54
ittadoru <ittadoru@gmail.com>
2025-08-06 15:15:57 +0300

    v2.0 release

--- handlers/user/download.py
+++ handlers/user/download.py
@@ -121,7 +121,7 @@ async def yt_download_callback(callback: types.CallbackQuery, state: FSMContext)
                     "720": 'bestvideo[ext=mp4][vcodec^=avc1][height<=720]+bestaudio[ext=m4a]/best[ext=mp4]',
                 }
                 file_path = await yt_dlp_dl.download(
-                    url, user.id, callback.message, custom_format=res_map_dl.get(res, res_map_dl["480"]), message=callback.message
+                    url, user.id, callback.message, custom_format=res_map_dl.get(res, res_map_dl["480"])
                 )
 
             width, height = get_video_resolution(file_path)
